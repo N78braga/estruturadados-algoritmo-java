79df46d9a48424e7bb43cf444dce2516a3ada60d
N78braga <nandobraga78@gmail.com>
2024-02-29 20:29:42 -0300

    Criando uma classe Paciente

--- src/com/nando/estruturadados/testeFila/Aula24.java
+++ src/com/nando/estruturadados/testeFila/Aula24.java
@@ -4,11 +4,15 @@ import com.nando.estruturadados.fila.FilaComPrioridade;
 
 public class Aula24 {
     public static void main(String[] args) {
-        FilaComPrioridade<Integer> fila = new FilaComPrioridade<>();
+        FilaComPrioridade<Paciente> fila = new FilaComPrioridade<>();
 
-        fila.enfileirar(1);
-        fila.enfileirar(3);
-        fila.enfileirar(2);
+        fila.enfileirar(new Paciente("A", 2));
+        fila.enfileirar(new Paciente("C", 1));
+        fila.enfileirar(new Paciente("B", 3));
+        System.out.println(fila);
+
+        System.out.println("Paciente retirado da fila " + fila.desenfileirar());
+
         System.out.println(fila);
     }
 }
